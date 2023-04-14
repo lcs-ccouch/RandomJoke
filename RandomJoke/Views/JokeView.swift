c372944eda0aa4646f3a013a167101669f655db8
Calum Couch <ccouch@lcs.on.ca>
2023-04-14 12:16:57 -0400

    Done the app

--- RandomJoke/Views/JokeView.swift
+++ RandomJoke/Views/JokeView.swift
@@ -10,35 +10,65 @@ import SwiftUI
 struct JokeView: View {
     @State var punchlineOpacity = 0.0
     // example joke
-    @State var Joke = exampleJoke1
+    @State var currentJoke: Joke?
     // MARK: computed properties
     var body: some View {
         NavigationView {
+            
             VStack{
-                Text(exampleJoke1.setup)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                
+                Spacer()
+                if let currentJoke = currentJoke {
+                    Text(currentJoke.setup)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    
+                    Button(action: {
+                        withAnimation(.easeIn(duration: 1.0)) {
+                            punchlineOpacity = 1.0
+                        }
+                    }, label: {
+                        Image(systemName: "arrow.down.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40)
+                            .tint(.black)
+                    })
+                    Text(currentJoke.punchline)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .opacity(punchlineOpacity)
+                    
+                }else {
+                    ProgressView()
+                }
+                Spacer()
+               
                 Button(action: {
-                    withAnimation(.easeIn(duration: 1.0)) {
-                        punchlineOpacity = 1.0
+                    // Reset the interface
+                    punchlineOpacity = 0.0
+
+                    Task {
+                        // Get another joke
+                        withAnimation {
+                            currentJoke = nil
+                        }
+                        currentJoke = await NetworkService.fetch()
                     }
                 }, label: {
-                    Image(systemName: "arrow.down.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 40)
-                        .tint(.black)
+                    Text("Fetch another one")
                 })
-                Text(exampleJoke1.punchline)
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .opacity(punchlineOpacity)
+                .disabled(punchlineOpacity == 0.0 ? true : false)
+                .buttonStyle(.borderedProminent)
+               	
                 
             }
             .navigationTitle("Random Jokes")
         }
+        .task { currentJoke = await NetworkService.fetch()
+        }
+        
     }
+        
 }
 
 struct JokeView_Previews: PreviewProvider {
