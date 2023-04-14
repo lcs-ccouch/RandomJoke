906c6f6ddbcee2f92864f40d1962079060eca4da
Calum Couch <ccouch@lcs.on.ca>
2023-04-14 11:11:26 -0400

    Initial Commit

---
+++ RandomJoke/RandomJokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomJokeApp.swift
+//  RandomJoke
+//
+//  Created by calum couch on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct RandomJokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
